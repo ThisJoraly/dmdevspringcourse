ef1aa4f8833d40a840aabed372c4f51a6a978c84
Artyom Khlebodarov <tyomajoraly@gmail.com>
2024-02-20 09:12:30 +0300

    dmdev course with querydsl

---
+++ common-logging-starter/src/main/java/com/dmdev/logging/aop/CommonPointcuts.java
@@ -0,0 +1,22 @@
+package com.dmdev.logging.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class CommonPointcuts {
+
+    /*
+        @within - check annotation on the class level
+     */
+    @Pointcut("@within(org.springframework.stereotype.Controller)")
+    public void isControllerLayer() {
+    }
+
+    /*
+        within - check class type name
+     */
+    @Pointcut("within(com.dmdev.*.service.*Service)")
+    public void isServiceLayer() {
+    }
+}
